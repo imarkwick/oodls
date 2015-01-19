54f2ef59e2a7d0835e669d8834fed3dcb05ce36f
imarkwick <izzymarkwick@gmail.com>
2015-01-19 21:34:00 +0000

    listing charities on charity page

--- app/controllers/charity_controller.rb
+++ app/controllers/charity_controller.rb
@@ -1,6 +1,19 @@
 class CharityController < ApplicationController
 
 	def index
+		@charities = Charity.all
 	end
+
+	def new
+	end
+
+	def create
+		@charity = Charity.create(params[:charity])
+		redirect_to '/charity'
+	end
+
+	# def charity_params
+	# 	params.require(:charity).permit(:organisation)
+	# end
 
 end
