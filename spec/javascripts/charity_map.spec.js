d4c3d2c294f533dca9ae35a84f2e3d3f1eb612d0
Alan Bridger <alanrbridger@gmail.com>
2015-01-20 18:41:55 +0000

    Test additions

--- spec/javascripts/charity_map.spec.js
+++ spec/javascripts/charity_map.spec.js
@@ -1,7 +1,11 @@
+// include http://maps.google.com/maps/api/js?sensor=true
+// include app/assets/javascripts/gmaps.js
 // include app/assets/javascripts/charity_map.js
+//= require gmaps
 //= require charity_map
 
 describe("user latitude and longitude", function() {
+
   it("should initially be undefined", function() {
     expect(userLatitude).toBe(undefined);
     expect(userLongitude).toBe(undefined);
