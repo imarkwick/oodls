61bd8520d68673813cbf46b22992a1ecdb4eedef
Claudia <claudia.donovan89@gmail.com>
2015-01-20 09:58:46 +0000

    trying to add codeclimate testing coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,8 @@
 #
 # The `.rspec` file also contains a few flags that are not defaults but that
 # users commonly want.
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
