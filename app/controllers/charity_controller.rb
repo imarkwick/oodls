35e2dfcd418c3e010c67c252d94f57a4430cbb73
Claudia <claudia.donovan89@gmail.com>
2015-01-21 21:32:21 +0000

    displaying requirements without a giant if statement but needs polishing

--- app/controllers/charity_controller.rb
+++ app/controllers/charity_controller.rb
@@ -7,6 +7,18 @@ class CharityController < ApplicationController
 		else
 			@current_charity = nil
 		end
+		
 	end
+
+	# def reqs_array
+	# 	food_reqs = %w(tins dried_goods coffee_tea fresh_fruit_veg snacks jars_condiments cereals cooking_ingredients drinks uht_milk)
+
+	# 	food_reqs.inject([]) do |memo, col|
+	# 		if (charity.send(col) == '1')
+	# 			memo << col.humanize
+	# 		end
+	# 		memo
+	# 	end
+	# end
 
 end
