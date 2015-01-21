a9d28fb1b55bc1988c24489732d6492434899a2f
Claudia <claudia.donovan89@gmail.com>
2015-01-21 12:26:55 +0000

    coords hashed and jsoned

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
 
 	def index
+		@charities = Charity.all
+		@hash = {}
+		@coords = @charities.each { |charity|
+			@hash[charity.id] = [charity.latitude, charity.longitude] 
+		}
+		@data = @hash.to_json
 	end
 	
 end
