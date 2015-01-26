2e6991cfacba09734789ca531e27314fd97b1cc7
Claudia <claudia.donovan89@gmail.com>
2015-01-26 10:55:04 +0000

    deleted

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,17 +3,5 @@ class HomeController < ApplicationController
 	def index
 		@charities_for_map = Charity.format_for_map
 	end
-	
-	def organisation
-		@organisation = Charity.find(params[:organisation])
-	end
-
-	def requirements
-		@requirements = Charity.requirements_array
-	end
-
-	def hours
-		@opening_hours = Charity.find
-	end
 
 end
