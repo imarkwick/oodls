a7c1993edfb34968f99e87137605e0b74d0e6f88
Claudia <claudia.donovan89@gmail.com>
2015-01-26 18:13:54 +0000

    refactored and tidied map page

--- app/assets/javascripts/map_index_page.js
+++ app/assets/javascripts/map_index_page.js
@@ -6,144 +6,6 @@ if (!navigator.geolocation) {
 
 var map;
 
-generateMap = function(latitude, longitude) {
-  map = new GMaps({
-    div: '#map',
-    lat: latitude,
-    lng: longitude,
-    zoom: 15,
-    disableDefaultUI: true
-  });
-};
-
-tescoImage = function(){
-  return image = {
-    url: 'images/tescomarker.png',
-    size: new google.maps.Size(27, 35),
-    origin: new google.maps.Point(0,0),
-    anchor: new google.maps.Point(14,35)
-  };
-};
-
-addTescoMarkers = function(){
-  $.getJSON('data/tescolonglat.json', function(json){
-    for(var i in json){
-      map.addMarker({
-        lat: json[i][0],
-        lng: json[i][1],
-        icon: tescoImage(),
-        infoWindow:{
-          content: '<p>Donation point</p>'
-        },
-        mouseover: function(event){
-          this.infoWindow.open(this.map, this);
-        },
-        mouseout: function(event){
-          this.infoWindow.close(this.map, this);
-        }
-      });
-    };
-  });
-};
-
-
-markerImage = function(url){
-  return image = {
-    url: url,
-    size: new google.maps.Size(20, 33),
-    origin: new google.maps.Point(0,0),
-    anchor: new google.maps.Point(10,33)
-  };
-};
-
-addUserMarker = function(latitude, longitude) {
-  map.addMarker({
-    lat: latitude,
-    lng: longitude
-  });
-};
-
-setUserPosition = function(latitude, longitude) {
-  userLatitude = latitude;
-  userLongitude = longitude;
-};
-
-getCharityData = function(){
-  var charity_data = $('.charity_data_class').data('charities-for-map');
-  assembleCharityMarkers(charity_data);
-};
-
-processCharityRequirements = function(i, charity_data){
-  return $.map(charity_data[i].requirements, function(req){
-    return req.label + "<img src='/images/icons/" + req.heading + ".svg' width='25' height='25'>";
-  });
-};
-
-renderInfoWindow = function(i, charity_data, requirements){
-  var html = $('#charity-info-window').html();
-  var data = {organisation: charity_data[i].organisation,
-              food_requirements: requirements,
-              weekday_hours: charity_data[i].weekday_hours,
-              weekend_hours: charity_data[i].weekend_hours,
-              id: charity_data[i].id
-            }
-  return Mustache.render(html,data);
-};
-
-addCharityMarkers = function(i, charity_data, charity_info){
-  map.addMarker({
-    lat: charity_data[i].lat,
-    lng: charity_data[i].lon,
-    icon: markerImage('images/oodls-pin-white.png'),
-    animation: google.maps.Animation.DROP,
-    infoWindow:{
-      content: charity_info
-    },
-    mouseover: function(event){
-      this.infoWindow.open(this.map, this);
-    }
-  });
-};
-
-assembleCharityMarkers = function(charity_data){
-  for(var i in charity_data){
-    var requirements = processCharityRequirements(i, charity_data).join(", ");
-    var charity_info = renderInfoWindow(i, charity_data, requirements);
-    addCharityMarkers(i, charity_data, charity_info);
-  };
-};
-
-assembleMap = function(postcode) {
-  GMaps.geocode({
-    address: postcode,
-    callback: function(results, status) {
-      if (status == 'OK') {
-        var latlng = results[0].geometry.location;
-        setUserPosition(latlng.lat(), latlng.lng())
-        hideSplashImages();
-        returnSearchBoxToTop();
-        generateMap(latlng.lat(), latlng.lng());
-        addUserMarker(latlng.lat(), latlng.lng());
-        addTescoMarkers();
-        getCharityData();
-        $("#postcode").css("border", "1px solid #cccccc");
-      }
-      else {
-        $("#postcode").css("border", "1px solid red");
-        $("#postcode").val('Please enter a valid address...');
-      }
-    }
-  });
-};
-
-fetchLocation = function() {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    setUserPosition(position.coords.latitude, position.coords.longitude)
-    var browserCoordinates = position.coords.latitude + ", " + position.coords.longitude
-    assembleMap(browserCoordinates);
-  });
-};
-
 returnSearchBoxToTop = function () {
   $(".search-box").addClass("align-search-box", 1000, "easeInOutCubic")
 };
@@ -168,3 +30,131 @@ $(window).on('resize', function(){
     map.setCenter(userLatitude, userLongitude);
   };
 });
+
+generateMap = function(latitude, longitude) {
+  map = new GMaps({
+    div: '#map',
+    lat: latitude,
+    lng: longitude,
+    zoom: 15,
+    disableDefaultUI: true
+  });
+};
+
+fetchLocation = function() {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    setUserPosition(position.coords.latitude, position.coords.longitude)
+    var browserCoordinates = position.coords.latitude + ", " + position.coords.longitude
+    assembleMap(browserCoordinates);
+  });
+};
+
+setUserPosition = function(latitude, longitude) {
+  userLatitude = latitude;
+  userLongitude = longitude;
+};
+
+addUserMarker = function(latitude, longitude) {
+  map.addMarker({
+    lat: latitude,
+    lng: longitude
+  });
+};
+
+markerImage = function(url, size_x, size_y, origin_x, origin_y, anchor_x, anchor_y){
+  return image = {
+    url: url,
+    size: new google.maps.Size(size_x, size_y),
+    origin: new google.maps.Point(origin_x, origin_y),
+    anchor: new google.maps.Point(anchor_x, anchor_y)
+  };
+};
+
+addTescoMarkers = function(){
+  $.getJSON('data/tescolonglat.json', function(json){
+    for(var i in json){
+      map.addMarker({
+        lat: json[i][0],
+        lng: json[i][1],
+        icon: markerImage('images/tescomarker.png', 27, 35, 0, 0, 14, 35),
+        infoWindow:{
+          content: '<p>Donation point</p>'
+        },
+        mouseover: function(event){
+          this.infoWindow.open(this.map, this);
+        },
+        mouseout: function(event){
+          this.infoWindow.close(this.map, this);
+        }
+      });
+    };
+  });
+};
+
+getCharityData = function(){
+  var charity_data = $('.charity_data_class').data('charities-for-map');
+  assembleCharityMarkers(charity_data);
+};
+
+processCharityRequirements = function(i, charity_data){
+  return $.map(charity_data[i].requirements, function(req){
+    return req.label + "<img src='/images/icons/" + req.heading + ".svg' width='25' height='25'>";
+  });
+};
+
+addCharityMarkers = function(i, charity_data, charity_info){
+  map.addMarker({
+    lat: charity_data[i].lat,
+    lng: charity_data[i].lon,
+    icon: markerImage('images/oodls-pin-white.png', 20, 33, 0, 0, 10, 33),
+    animation: google.maps.Animation.DROP,
+    infoWindow:{
+      content: charity_info
+    },
+    mouseover: function(event){
+      this.infoWindow.open(this.map, this);
+    }
+  });
+};
+
+assembleCharityMarkers = function(charity_data){
+  for(var i in charity_data){
+    var requirements = processCharityRequirements(i, charity_data).join(", ");
+    var charity_info = renderInfoWindow(i, charity_data, requirements);
+    addCharityMarkers(i, charity_data, charity_info);
+  };
+};
+
+renderInfoWindow = function(i, charity_data, requirements){
+  var html = $('#charity-info-window').html();
+  var data = {organisation: charity_data[i].organisation,
+              food_requirements: requirements,
+              weekday_hours: charity_data[i].weekday_hours,
+              weekend_hours: charity_data[i].weekend_hours,
+              id: charity_data[i].id
+            }
+  return Mustache.render(html,data);
+};
+
+assembleMap = function(postcode) {
+  GMaps.geocode({
+    address: postcode,
+    callback: function(results, status) {
+      if (status == 'OK') {
+        var latlng = results[0].geometry.location;
+        setUserPosition(latlng.lat(), latlng.lng())
+        hideSplashImages();
+        returnSearchBoxToTop();
+        generateMap(latlng.lat(), latlng.lng());
+        addUserMarker(latlng.lat(), latlng.lng());
+        addTescoMarkers();
+        getCharityData();
+        $("#postcode").css("border", "1px solid #cccccc");
+      }
+      else {
+        $("#postcode").css("border", "1px solid red");
+        $("#postcode").val('Please enter a valid address...');
+      }
+    }
+  });
+};
