a4d16b23fc4655db40aee484ac02044865cd1d94
imarkwick <izzymarkwick@gmail.com>
2015-01-27 17:57:33 +0000

    adding donor spec tests

---
+++ spec/features/donor_feature_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+require_relative 'charity_helper'
+
+include CharityHelper
+
+feature 'Donor' do
+
+	context 'looking to make a donation' do
+
+		scenario 'should be able to view all the charities signed up' do
+			multi_sign_up
+			visit '/'
+			click_link 'Where to give?'
+			expect(page).to have_content 'All Charities'
+			expect(page).to have_content 'Crisis'
+			expect(page).to have_content 'Second Charity'
+		end
+
+		scenario 'should be able to visit the website' do
+			sign_up_with_website
+			click_link 'Where to give?'
+			expect(page).to have_link 'Visit the website'
+			click_link 'Visit the website'
+			expect(current_url).to eq 'http://www.crisis.org.uk/'
+		end
+
+		scenario 'should be able to find out donation guidelines' do
+			visit '/'
+			click_link 'How to give?'
+			expect(page).to have_content 'Donation Guidelines'
+		end
+
+		scenario 'should be able to see if there is more they can do' do
+			visit '/'
+			click_link 'What more can I do?'
+			expect(page).to have_content 'What else can I do?'
+		end
+
+	end
+
+end
