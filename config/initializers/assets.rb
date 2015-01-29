5817c4e9db135ddfdc0dd918b92af03fbe38897f
Claudia <claudia.donovan89@gmail.com>
2015-01-29 17:35:13 +0000

    select all

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,10 +5,8 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+# Rails.application.config.assets.precompile += %w( checkboxes.js )
 Rails.application.config.assets.precompile += %w( gmaps.js )
-
-# Rails.application.config.assets.precompile += %w( charity_map.js )
 
 Rails.application.config.assets.precompile += %w( map_index_page.js )
 
